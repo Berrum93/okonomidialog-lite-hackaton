a0e3947469e328efa1d9b5b37a34d31d95a1fe08
Tor Seivaag Knudsen <TorSeivaag.Knudsen@skatteetaten.no>
2023-11-29 11:12:53 +0100

    Remove total expenses calculation from Utgifter component

--- src/components/utgifter.tsx
+++ src/components/utgifter.tsx
@@ -20,9 +20,6 @@ export const Utgifter: FC = () => {
 
   return (
     <div>
-      <div>
-        <p>Totale utgifter: {data?.utgifter.reduce((acc, utgift) => acc + utgift.belop, 0)}</p>
-      </div>
       {data?.utgifter?.map((utgift, index) => {
         return (
           <div key={index}>
