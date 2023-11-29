51c2e17dea8c608b4370d2e51b5e640d0412b97d
Anders Fjellheim Berrum <AndersFjellheim.Berrum@skatteetaten.no>
2023-11-29 09:40:46 +0100

    La til utgifter

--- src/components/utgifter.tsx
+++ src/components/utgifter.tsx
@@ -1,6 +1,36 @@
-import { FC } from 'react';
-// import { Utgift } from '../api/types';
+import { ChangeEvent, FC } from "react";
+import { useAtom } from "jotai";
+import { dataAtom } from "../App"; // import the atom from where it's defined
+import { TextField } from "@skatteetaten/ds-forms";
 
 export const Utgifter: FC = () => {
-  return <div>{'Utgifter'}</div>;
+  const [data, setData] = useAtom(dataAtom); // use the atom in your component
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { value } = event.target;
+    if (data !== null) {
+      setData({ ...data, age: value as unknown as number });
+    }
+  };
+
+  return (
+    <div>
+      <h2>Utgifter</h2>
+      {data?.utgifter?.map((utgift, index) => (
+        <div key={index}>
+          <p>Type: {utgift.type}</p>
+          <p>Beløp: {utgift.belop}</p>
+          <p>Beløp: {data.age}</p>
+        </div>
+      ))}
+      <TextField
+        label="Type"
+        onChange={handleInputChange}
+        thousandSeparator={true}
+        placeholder="Hei"
+        value={data?.age}
+      />
+    </div>
+  );
 };
