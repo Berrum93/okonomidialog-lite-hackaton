54b3b116ab46aa14ac24a24387e427657b451953
Anders Fjellheim Berrum <AndersFjellheim.Berrum@skatteetaten.no>
2023-11-29 13:57:35 +0100

    La til totale utgifter / inntekter

--- src/components/inntekter.tsx
+++ src/components/inntekter.tsx
@@ -1,51 +1,58 @@
 import React from "react";
 import { useAtom } from "jotai";
-import { dataAtom } from "../App";
-import { TextField } from '@skatteetaten/ds-forms';
+import { dataAtom, totalInntektAtom } from "../App";
+import { TextField } from "@skatteetaten/ds-forms";
 import { DataModel } from "../api/types";
 
 const Inntekter: React.FC = () => {
   const [data, setData] = useAtom(dataAtom); // use the atom in your component
+  const [totalInntekter] = useAtom(totalInntektAtom);
 
   const handleChange = (value: string, id: string) => {
     setData((prev) => {
       if (!prev) return null;
 
       const newData: DataModel = { ...prev };
-      newData.inntekter = newData.inntekter?.map((inntekt) => {
-        if (inntekt.type === id) {
-          const parsedValue = parseInt(value.replace(/\s/g, ""));
-          return { ...inntekt, belop: isNaN(parsedValue) ? 0 : parsedValue };
-        }
-        return inntekt;
-      }) ?? [];
+      newData.inntekter =
+        newData.inntekter?.map((inntekt) => {
+          if (inntekt.type === id) {
+            const parsedValue = parseInt(value.replace(/\s/g, ""));
+            return { ...inntekt, belop: isNaN(parsedValue) ? 0 : parsedValue };
+          }
+          return inntekt;
+        }) ?? [];
       return newData;
     });
-  }
+  };
 
   return (
     <div>
-      <div>
-        <p>Totale inntekter: {data?.inntekter.reduce((acc, inntekt) => acc + inntekt.belop, 0)}</p>
-      </div>
       {data?.inntekter?.map((inntekt, index) => (
         <div key={index}>
           <p>Inntekt: {inntekt.type}</p>
-          <p>Beløp: 
+          <p>
+            Beløp:
             <div>
               <TextField
                 id="inntekt"
                 label="Inntekt"
                 placeholder="Skriv inn inntekt"
-                onChange={(e) => {handleChange(e.target.value, inntekt.type)}}
-                value={data?.inntekter?.find((i) => i.type === inntekt.type)?.belop}
+                onChange={(e) => {
+                  handleChange(e.target.value, inntekt.type);
+                }}
+                value={
+                  data?.inntekter?.find((i) => i.type === inntekt.type)?.belop
+                }
                 thousandSeparator
               />
             </div>
           </p>
         </div>
-       
       ))}
+      <br />
+      <strong style={{ fontSize: "larger" }}>
+        Totale inntekter: {totalInntekter}
+      </strong>
     </div>
   );
 };
